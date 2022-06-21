30d14499cc45be7d65482777bd8f80f77b9d4c44
alex-korzh <alex@korzh.me>
2022-06-21 20:35:59 +0300

    versioning

---
+++ src/main/java/name/dezalator/util/Config.java
@@ -0,0 +1,26 @@
+package name.dezalator.util;
+
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
+public class Config {
+    public String version;
+
+    public Config() {
+        Parameters params = new Parameters();
+        FileBasedConfigurationBuilder<FileBasedConfiguration> builder =
+                new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
+                        .configure(params.properties()
+                                .setFileName("application.properties"));
+        try
+        {
+            Configuration config = builder.getConfiguration();
+            version = config.getString("version");
+        }
+        catch(ConfigurationException ignored) {}
+    }
+}
